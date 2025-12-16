c477bfbd9fae353c31568b878f745fe8ae6743d0
jmirensky <jmirensky01@gmail.com>
2025-12-16 14:01:47 +0200

    excercises XPGold week1, day2

---
+++ W1/day2/ExcercisesXPGold/excercise1.py
@@ -0,0 +1,12 @@
+#Print the following output in one line of code:
+
+#Hello world
+#Hello world
+#Hello world
+#Hello world
+#I love python
+#I love python
+#I love python
+#I love python
+
+print ("hello world\n"*4 + "I love python\n"*4)
