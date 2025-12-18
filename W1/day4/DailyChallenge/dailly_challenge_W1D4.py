4c062de91ab7a83b763fc86334c7988374f17ada
jmirensky <jmirensky01@gmail.com>
2025-12-18 17:14:46 +0200

    dailly challenge week1, day 4

---
+++ W1/day4/DailyChallenge/dailly_challenge_W1D4.py
@@ -0,0 +1,40 @@
+#Daily Challenge : Lists & Strings
+
+#Challenge 1: Multiples of a Number
+#1. Ask the user for two inputs:
+    #A number (integer).
+    #A length (integer).
+
+number = int(input('Choose a number: '))   
+lenght = int(input('Choose the lenght: ')) 
+
+
+#2. Create a program that generates a list of multiples of the given number.
+#3. The list should stop when it reaches the length specified by the user.
+
+list_multiples = []
+for l in range(1, lenght+1):
+    list_multiples.append(number * l)
+print (list_multiples)
+
+
+#Challenge 2: Remove Consecutive Duplicate Letters
+
+#1. Ask the user for a string.
+#2. Write a program that processes the string to remove consecutive duplicate letters.
+    #The new string should only contain unique consecutive letters.
+    #For example, “ppoeemm” should become “poem” (removes consecutive duplicates like ‘pp’, ‘ee’, and ‘mm’).
+#3. The program should print the modified string.
+
+#LO HICE CON CHATGTP, Hard excercise
+
+word = input("Enter a word: ")
+
+result = ""
+
+for char in word:
+    if result == "" or char != result[-1]:
+        result += char
+
+print(result)
+
