63d0a9e2ce16a0f223f6e744d27fe9c45f474dc7
jmirensky <jmirensky01@gmail.com>
2026-01-28 14:29:52 +0200

    Daily Challenge W3D4

--- W3/day4/DailyChallenge/DailyChallengeW3D4_Circle.py
+++ W3/day4/DailyChallenge/DailyChallengeW3D4_Circle.py
@@ -2,6 +2,7 @@
 #goal = create a class that represents a simple circle.
 
 import math
+import turtle
 
 class Circle:
 
@@ -81,3 +82,26 @@ for c in circles:
     # Circle (radius = 13.0)
     # Circle (radius = 20)
     # Circle (radius = 40)
+
+
+# Bonus Challenge  
+#Draw Circles
+
+screen = turtle.Screen()
+screen.title("Sorted Circles")
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.width(2)
+
+x_position = -150  # start drawing from the left
+
+for circle in circles:
+    pen.penup()
+    pen.goto(x_position, 0)
+    pen.pendown()
+    pen.circle(circle.radius)
+
+    x_position += circle.radius * 2 + 20  # space between circles
+
+screen.mainloop()
